5d05b0475939ff46a3938df0bd9f740a47e6c69c
Tining <1340212468@qq.com>
2019-09-19 23:04:03 +0930

    19/9/2019 Log update

--- src/com/tining/littleone/tools/StringTools.java
+++ src/com/tining/littleone/tools/StringTools.java
@@ -10,16 +10,31 @@ import java.util.ArrayList;
  * @Version 1.0
  **/
 public class StringTools extends Tools{
+    /*
+    *@Author Tining
+    *@Description 把字符串按行转成字符串表
+    *@Date 2019/9/19 22:09 
+    *@Param [str]
+    *@return java.util.ArrayList<java.lang.String>
+    **/
     public static ArrayList<String> lineToList(String str){
         String[] arr = str.split("\n");
         return arrayToList(arr);
 
     }
 
+    /*
+    *@Author Tining
+    *@Description
+    *@Date 2019/9/19 22:11
+    *@Param [arr]
+    *@return java.util.ArrayList<java.lang.String>
+    **/
     public static ArrayList<String> arrayToList(String[] arr){
         ArrayList<String> list = new ArrayList<String>();
         for(int i = 0 ; i < arr.length;i++)
             list.add(arr[i]);
         return list;
     }
+
 }
--- src/com/tining/littleone/tools/log/Log.java
+++ src/com/tining/littleone/tools/log/Log.java
@@ -1,13 +1,69 @@
 package com.tining.littleone.tools.log;
 
 public abstract class Log {
+    ///默认消息等级
+    public int defaultLevel = 0;
+
+    /*
+    *@Author Tining
+    *@Description 返回默认消费等级
+    *@Date 2019/9/19 22:32
+    *@Param []
+    *@return int
+    **/
+    public int getDefaultLevel(){return  this.defaultLevel;}
+
+    /*
+    *@Author Tining
+    *@Description 设置默认消息等级
+    *@Date 2019/9/19 22:33
+    *@Param [level]
+    *@return void
+    **/
+    public void setDefaultLevel(int level){this.defaultLevel = level;}
+
+    /*
+    *@Author Tining
+    *@Description 默认构造函数
+    *@Date 2019/9/19 23:01 
+    *@Param []
+    *@return 
+    **/
+    public Log(){}
+
+    /*
+    *@Author Tining
+    *@Description 设置默认消息级别的构造函数
+    *@Date 2019/9/19 23:02 
+    *@Param [defaultLevel]
+    *@return 
+    **/
+    public Log(int defaultLevel){this.defaultLevel = defaultLevel;}
+    
     /*
      *@Author Tining
-     *@Description 记录日志
+     *@Description 记录日志，采用异常类型
      *@Date 2019/9/18 1:47
      *@Param [obj]
      *@return void
      **/
-    public abstract void log(Object obj);
+    public abstract void log(Exception e);
 
+    /*
+     *@Author Tining
+     *@Description 记录日志，采用字符串消息
+     *@Date 2019/9/18 1:47
+     *@Param [obj]
+     *@return void
+     **/
+    public void log(String msg){log(msg,this.defaultLevel);}
+
+    /*
+     *@Author Tining
+     *@Description 记录日志，采用字符串消息，并且设定消息等级
+     *@Date 2019/9/18 1:47
+     *@Param [obj]
+     *@return void
+     **/
+    public abstract void log(String msg, int msgLevel);
 }
